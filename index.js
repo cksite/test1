1a387c8f99a361e98a207faef4baf8d1ebb82577
Chandan Kumar Saw <chandan1951c@gmail.com>
2023-07-03 15:18:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function openlink()
+{
+    var txt = document.getElementById("inptxt").value;
+    if(txt == 'asit')
+    {
+        window.location.href = "https://www.geeksforgeeks.org/";
+    }
+    else
+    {
+        alert("oho,wrong password");
+    }
+
+}
+
