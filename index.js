2d050913e97a3b7cd68bdc132315de8beaa0b7b7
Chandan Kumar Saw <chandan1951c@gmail.com>
2023-07-03 15:20:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function openlink()
     var txt = document.getElementById("inptxt").value;
     if(txt == 'asit')
     {
-        window.location.href = "https://www.geeksforgeeks.org/";
+        window.location.href = "https://chrome.google.com/webstore/detail/save-link/ilnignejhkdolncbhpebpdnedknddgfp?hl=en&authuser=6";
     }
     else
     {
